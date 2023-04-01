906265e4847bd9c561130111ab7139c6d01e3341
Shahar Yair <56793023+shaharyair@users.noreply.github.com>
2023-04-01 19:44:41 +0300

    added video background and responsive text

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,17 +1,14 @@
 import React from "react";
 import "./Hero.css";
-import { Link } from "react-router-dom";
 import { FaWhatsapp, FaPhoneAlt } from "react-icons/fa";
-import video1 from "/Users/shaharyair/React Projects/Katrin Fitness landing page/vite-project/src/assets/video1.mp4";
-import video2 from "/Users/shaharyair/React Projects/Katrin Fitness landing page/vite-project/src/assets/video2.mp4";
-import video3 from "/Users/shaharyair/React Projects/Katrin Fitness landing page/vite-project/src/assets/video3.mp4";
+import bgVideo from "/Users/shaharyair/React Projects/Katrin Fitness landing page/vite-project/src/assets/video2.mp4";
 
 function Hero() {
   return (
     <>
       <div className='hero' id='hero'>
-        <video src={video2} autoPlay muted loop className='hero__video' />
-        <div className='hero__container'>
+        <video src={bgVideo} autoPlay muted loop className='hero__video' />
+        <div className='hero__text-container'>
           <div className='hero__text-head'>
             <h1>קאתרין</h1>
           </div>
@@ -20,12 +17,12 @@ function Hero() {
             <p>להתמלא בעוצמה</p>
           </div>
           <div className='hero__buttons-container'>
-            <Link to='https://wa.me/972556620441' className='hero__button'>
+            <a href='https://wa.me/972556620441' className='hero__button'>
               לפרטים נוספים <FaWhatsapp className='hero__button-icon' />
-            </Link>
-            <Link to='tel:+972556620441' className='hero__button'>
+            </a>
+            <a href='tel:+972556620441' className='hero__button'>
               חייגו אלינו <FaPhoneAlt className='hero__button-icon' />
-            </Link>
+            </a>
           </div>
         </div>
       </div>
