f739020a72fe9ca8c31b7bd7b21e46812c8eae79
Shahar Yair <56793023+shaharyair@users.noreply.github.com>
2023-04-06 14:59:01 +0300

    restored app.jsx manually

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar/Navbar";
+import Hero from "./Components/Hero/Hero";
+import WhatsappButton from "./Components/WhatsappButton/WhatsappButton";
+import Studio from "./Components/Studio/Studio";
+import FAQ from "./Components/FAQ/FAQ";
+import Benefits from "./Components/Benefits/Benefits";
+import ImgSlider from "./Components/ImgSlider/ImgSlider";
+
+function App() {
+  return (
+    <>
+      <Router></Router>
+    </>
+  );
+}
+
+export default App;
