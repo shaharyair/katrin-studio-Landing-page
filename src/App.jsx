972d661c13f3341e64d071c690b74ad0488eeecb
Shahar Yair <56793023+shaharyair@users.noreply.github.com>
2023-04-06 15:57:07 +0300

    removed variables.css and added vars to index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar/Navbar";
 import Hero from "./Components/Hero/Hero";
 import WhatsappButton from "./Components/WhatsappButton/WhatsappButton";
@@ -10,11 +10,7 @@ import Benefits from "./Components/Benefits/Benefits";
 import ImgSlider from "./Components/ImgSlider/ImgSlider";
 
 function App() {
-  return (
-    <>
-      <Router></Router>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
