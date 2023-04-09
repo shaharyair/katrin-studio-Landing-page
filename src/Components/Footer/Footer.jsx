6c7abf248ec1c82547d60faf951ee65c07df1490
Shahar Yair <shaharyair12@gmail.com>
2023-04-09 20:59:11 +0300

    added style and responsive layout

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,40 +1,68 @@
 import React from "react";
 import "./Footer.css";
 import { Link as ScrollLink } from "react-scroll";
+import { AiOutlineMail } from "react-icons/ai";
 
 function Footer() {
   return (
     <>
       <div className='footer'>
         <div className='footer__container'>
-          <nav className='footer__nav'>
+          <div className='footer__nav'>
+            <h1 className='footer-title'>תפריט ניווט</h1>
+            <nav>
+              <ul>
+                <li>
+                  <ScrollLink
+                    to='hero'
+                    spy={true}
+                    smooth={true}
+                    offset={-70}
+                    duration={500}
+                    className='footer__nav-link'
+                  >
+                    ראשי
+                  </ScrollLink>
+                </li>
+                <li>
+                  <ScrollLink
+                    to='studio'
+                    spy={true}
+                    smooth={true}
+                    offset={-70}
+                    duration={500}
+                    className='footer__nav-link'
+                  >
+                    הסטודיו
+                  </ScrollLink>
+                </li>
+                <li>
+                  <ScrollLink
+                    to='faq'
+                    spy={true}
+                    smooth={true}
+                    offset={-70}
+                    duration={500}
+                    className='footer__nav-link'
+                  >
+                    שאלות נפצות
+                  </ScrollLink>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <div className='footer-info'>
+            <h1 className='footer-title'>ליצירת קשר</h1>
             <ul>
-              <li className='footer__nav-item'>
-                <ScrollLink
-                  to='hero'
-                  spy={true}
-                  smooth={true}
-                  offset={-70}
-                  duration={500}
-                  className='footer__nav-link'
-                >
-                  ראשי
-                </ScrollLink>
-              </li>
-              <li className='footer__nav-item'>
-                <ScrollLink
-                  to='studio'
-                  spy={true}
-                  smooth={true}
-                  offset={-70}
-                  duration={500}
-                  className='footer__nav-link'
-                >
-                  הסטודיו
-                </ScrollLink>
+              <li className='footer-info-item'>055-6520441</li>
+              <li className='footer-info-item'>הגלבוע 41, עפולה</li>
+              <li className='footer-info-item'>
+                <AiOutlineMail className='footer-icon' />
+                katrinyair@gmail.com
               </li>
             </ul>
-          </nav>
+          </div>
+
           <ScrollLink
             to='hero'
             spy={true}
