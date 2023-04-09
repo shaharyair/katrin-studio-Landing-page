692566940c0a6ed71d82a46b3ec5ca954acfabe1
Shahar Yair <shaharyair12@gmail.com>
2023-04-09 13:27:03 +0300

    removed labels

--- src/Components/SignupForm/SignupForm.jsx
+++ src/Components/SignupForm/SignupForm.jsx
@@ -25,22 +25,22 @@ const SignupForm = () => {
   return (
     <form onSubmit={handleSubmit} className='form-container'>
       <div className='form-field'>
-        <label htmlFor='fullName'>שם מלא</label>
         <input
           type='text'
           id='fullName'
           name='fullName'
+          placeholder='שם מלא'
           value={formData.fullName}
           onChange={handleChange}
           required
         />
       </div>
       <div className='form-field'>
-        <label htmlFor='phoneNumber'>מספר טלפון</label>
         <input
           type='tel'
           id='phoneNumber'
           name='phoneNumber'
+          placeholder='מספר טלפון'
           pattern='[\d-]*'
           input='numeric'
           value={formData.phoneNumber}
