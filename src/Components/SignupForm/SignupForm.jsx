f9f2a1edb86d9cdc75c398cecbd89ea47c02a448
Shahar Yair <shaharyair12@gmail.com>
2023-04-09 13:14:26 +0300

    improved styling and added responsiveness

--- src/Components/SignupForm/SignupForm.jsx
+++ src/Components/SignupForm/SignupForm.jsx
@@ -4,9 +4,7 @@ import "./SignupForm.css";
 const SignupForm = () => {
   const [formData, setFormData] = useState({
     fullName: "",
-    email: "",
     phoneNumber: "",
-    fitnessLevel: "",
   });
 
   const handleChange = (event) => {
@@ -17,16 +15,11 @@ const SignupForm = () => {
   };
 
   const handleSubmit = (event) => {
-    event.preventDefault();
-    axios.post("YOUR_EMAIL_API_ENDPOINT", formData).then((response) => {
-      console.log(response.data);
-    });
-    setFormData({
-      fullName: "",
-      email: "",
-      phoneNumber: "",
-      fitnessLevel: "",
-    });
+    // event.preventDefault();
+    // setFormData({
+    //   fullName: "",
+    //   phoneNumber: "",
+    // });
   };
 
   return (
@@ -43,17 +36,6 @@ const SignupForm = () => {
         />
       </div>
       <div className='form-field'>
-        <label htmlFor='email'>כתובת מייל</label>
-        <input
-          type='email'
-          id='email'
-          name='email'
-          value={formData.email}
-          onChange={handleChange}
-          required
-        />
-      </div>
-      <div className='form-field'>
         <label htmlFor='phoneNumber'>מספר טלפון</label>
         <input
           type='tel'
@@ -65,21 +47,6 @@ const SignupForm = () => {
           onChange={handleChange}
           required
         />
-      </div>
-      <div className='form-field'>
-        <label htmlFor='fitnessLevel'>רמת כושר</label>
-        <select
-          id='fitnessLevel'
-          name='fitnessLevel'
-          value={formData.fitnessLevel}
-          onChange={handleChange}
-          required
-        >
-          <option value=''>בחר רמת כושר</option>
-          <option value='מתחיל'>מתחיל</option>
-          <option value='בינוני'>בינוני</option>
-          <option value='מתקדם'>מתקדם</option>
-        </select>
       </div>
       <button type='submit'>שלח</button>
     </form>
