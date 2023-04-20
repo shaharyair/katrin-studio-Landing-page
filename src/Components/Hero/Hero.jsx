f9e2d99067880d54dcf7b1fa3a32e2c0c11c9093
Shahar Yair <shaharyair12@gmail.com>
2023-04-20 15:36:25 +0300

    changed to a gradient background and removed video

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -2,23 +2,24 @@ import React from "react";
 import "./Hero.css";
 import { FaWhatsapp, FaPhoneAlt } from "react-icons/fa";
 import bgVideo from "/Users/shaharyair/React Projects/Katrin Landing Page Project/katrin-project-client/src/assets/video2.mp4";
+import pic from "/src/assets/girlexample.png";
 
 function Hero() {
   return (
     <>
       <div className='hero' id='hero'>
-        <video
+        {/* <video
           className='hero__video'
           src={bgVideo}
           muted
           playsInline
           loop
           autoPlay
-        />
-
+        /> */}
+        <img src={pic} alt='' />
         <div className='hero__text-container'>
           <div className='hero__text-head'>
-            <h1>קאתרין</h1>
+            <h1>הסטודיו של קאתרין</h1>
           </div>
           <div className='hero__text'>
             <p>להרגיש בבית</p>
