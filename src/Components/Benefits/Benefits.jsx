883c78a4ac1871b42c7930195e8d35002fa60940
Shahar Yair <shaharyair12@gmail.com>
2023-04-21 18:54:10 +0300

    added animations

--- src/Components/Benefits/Benefits.jsx
+++ src/Components/Benefits/Benefits.jsx
@@ -1,52 +1,64 @@
-import React from "react";
+import { React, useState } from "react";
 import "./Benefits.css";
 import Pic1 from "/src/assets/benefits1.jpg";
 import Pic2 from "/src/assets/benefits2.jpg";
 import Pic3 from "/src/assets/benefits4.jpg";
-import Pic4 from "/src/assets/benefits3.jpg";
+import ScrollTrigger from "react-scroll-trigger";
 
 function Benefits() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleEnter = () => {
+    setIsVisible(true);
+  };
+
   return (
     <>
       <div className='benefits'>
         <h1>
           <span>בסטודיו של קאתרין</span> - אנחנו עובדים בשיטה שמובילה להצלחה
         </h1>
-        <div className='benefits-container'>
-          <div className='benefit'>
-            <img src={Pic1} alt='/' />
-            <div className='benefits__text-container'>
-              <h1 className='benefits-title'>ליווי אישי להצלחה</h1>
-              <p className='benefits-content'>
-                המתכון לחיטוב פלג הגוף התחתון, שמשלב בתוכו תזונה, תרגילים
-                ייעודיים, טכניקה ומספר חזרות, נמצא בקורס הדיגיטלי לעיצוב הישבן
-                והירכיים
-              </p>
+        <ScrollTrigger onEnter={handleEnter}>
+          <div
+            className={`benefits-container ${
+              isVisible ? "benefits-scale-forwards" : ""
+            }`}
+          >
+            <div className='benefit'>
+              <img src={Pic1} alt='/' />
+              <div className='benefits__text-container'>
+                <h1 className='benefits-title'>ליווי אישי להצלחה</h1>
+                <p className='benefits-content'>
+                  המתכון לחיטוב פלג הגוף התחתון, שמשלב בתוכו תזונה, תרגילים
+                  ייעודיים, טכניקה ומספר חזרות, נמצא בקורס הדיגיטלי לעיצוב הישבן
+                  והירכיים
+                </p>
+              </div>
+            </div>
+            <div className='benefit'>
+              <img src={Pic2} alt='/' />
+              <div className='benefits__text-container'>
+                <h1 className='benefits-title'>ליווי אישי להצלחה</h1>
+                <p className='benefits-content'>
+                  המתכון לחיטוב פלג הגוף התחתון, שמשלב בתוכו תזונה, תרגילים
+                  ייעודיים, טכניקה ומספר חזרות, נמצא בקורס הדיגיטלי לעיצוב הישבן
+                  והירכיים
+                </p>
+              </div>
+            </div>
+            <div className='benefit'>
+              <img src={Pic3} alt='/' />
+              <div className='benefits__text-container'>
+                <h1 className='benefits-title'>ליווי אישי להצלחה</h1>
+                <p className='benefits-content'>
+                  המתכון לחיטוב פלג הגוף התחתון, שמשלב בתוכו תזונה, תרגילים
+                  ייעודיים, טכניקה ומספר חזרות, נמצא בקורס הדיגיטלי לעיצוב הישבן
+                  והירכיים
+                </p>
+              </div>
             </div>
           </div>
-          <div className='benefit'>
-            <img src={Pic2} alt='/' />
-            <div className='benefits__text-container'>
-              <h1 className='benefits-title'>ליווי אישי להצלחה</h1>
-              <p className='benefits-content'>
-                המתכון לחיטוב פלג הגוף התחתון, שמשלב בתוכו תזונה, תרגילים
-                ייעודיים, טכניקה ומספר חזרות, נמצא בקורס הדיגיטלי לעיצוב הישבן
-                והירכיים
-              </p>
-            </div>
-          </div>
-          <div className='benefit'>
-            <img src={Pic3} alt='/' />
-            <div className='benefits__text-container'>
-              <h1 className='benefits-title'>ליווי אישי להצלחה</h1>
-              <p className='benefits-content'>
-                המתכון לחיטוב פלג הגוף התחתון, שמשלב בתוכו תזונה, תרגילים
-                ייעודיים, טכניקה ומספר חזרות, נמצא בקורס הדיגיטלי לעיצוב הישבן
-                והירכיים
-              </p>
-            </div>
-          </div>
-        </div>
+        </ScrollTrigger>
       </div>
     </>
   );
