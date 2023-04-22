25f5dffd6f5ce68cdd7f395a459c460679db009e
Shahar Yair <shaharyair12@gmail.com>
2023-04-22 16:16:35 +0300

    improving hero structure

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -7,22 +7,24 @@ function Hero() {
   return (
     <>
       <div className='hero' id='hero'>
-        <img src={pic} alt='' />
-        <div className='hero__text-container'>
-          <div className='hero__text-head'>
-            <h1>הסטודיו של קאתרין</h1>
-          </div>
-          <div className='hero__text'>
-            <p>להרגיש בבית</p>
-            <p>להתמלא בעוצמה</p>
-          </div>
-          <div className='hero__buttons-container'>
-            <a href='https://wa.me/972556620441' className='hero__button'>
-              לפרטים נוספים <FaWhatsapp className='hero__button-icon' />
-            </a>
-            <a href='tel:+972556620441' className='hero__button'>
-              חייגו אלינו <FaPhoneAlt className='hero__button-icon' />
-            </a>
+        <div className='hero__container'>
+          <img src={pic} alt='' />
+          <div className='hero__text-container'>
+            <div className='hero__text-head'>
+              <h1>הסטודיו של קאתרין</h1>
+            </div>
+            <div className='hero__text'>
+              <p>להרגיש בבית</p>
+              <p>להתמלא בעוצמה</p>
+            </div>
+            <div className='hero__buttons-container'>
+              <a href='https://wa.me/972556620441' className='hero__button'>
+                לפרטים נוספים <FaWhatsapp className='hero__button-icon' />
+              </a>
+              <a href='tel:+972556620441' className='hero__button'>
+                חייגו אלינו <FaPhoneAlt className='hero__button-icon' />
+              </a>
+            </div>
           </div>
         </div>
       </div>
