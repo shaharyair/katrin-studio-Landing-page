b57c6457e3b7a6801b0af4c01c9fbe461a4ee2ec
Shahar Yair <shaharyair12@gmail.com>
2023-04-26 11:33:27 +0300

    updated text

--- src/Components/Recipes/Recipes.jsx
+++ src/Components/Recipes/Recipes.jsx
@@ -9,7 +9,7 @@ const Recipes = () => {
           <h1>הצטרפי לקהילת המתכונים שלנו!</h1>
           <button>
             <a href='https://chat.whatsapp.com/LNe0EqWlJStCAtybKVw3Rm'>
-              לחצי כאן
+              הצטרפי עכשיו
             </a>
           </button>
         </div>
--- src/Components/Studio/Studio.jsx
+++ src/Components/Studio/Studio.jsx
@@ -22,14 +22,16 @@ function Studio() {
             <div className='studio__text-container'>
               <h1>הסטודיו</h1>
               <p>
-                צריך לכתוב Lorem ipsum dolor sit amet, consectetur adipisicing
-                elit. Eligendi nulla consectetur necessitatibus nobis deleniti
-                eum, modi maxime nemo cum. Labore?
+                בסטודיו מאוד חשוב לי ליצור אווירה תומכת ומקבלת שתרגום לכן להרגיש
+                כמו הבית השני שלכן. כמאמנת אני מחויבת לעזור לכן להגיע ליעדים
+                שהצבנו, בין אם אתן בכושר שיא או בתחילת התהליך אנחנו ניצור תוכנית
+                שמותאם לך אישית שנוכל למקסם את הפוטנציאל שלך ולהגיע לגוף
+                חלומותיך.
               </p>
               <p>
-                צריך לכתוב Lorem, ipsum dolor sit amet consectetur adipisicing
-                elit. Eius doloremque dolorum ratione id impedit atque officia
-                cumque ducimus beatae tempora?
+                אני מאמינה שכשיש לך מקום, חזון וליווי תוכלי להגשים כל דבר שרק
+                תרצי. אני מזמינה אותך לקחת את הצעד הראשון לחיים בריאים ושמחים
+                יותר.
               </p>
             </div>
             <img src={studioPng} alt='' />
