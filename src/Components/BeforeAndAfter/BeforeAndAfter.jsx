e50efa2e40e40d9df76d698f4b26771b8c485e6a
Shahar Yair <shaharyair12@gmail.com>
2023-04-27 21:22:39 +0300

    updated photos and improved style

--- src/Components/BeforeAndAfter/BeforeAndAfter.jsx
+++ src/Components/BeforeAndAfter/BeforeAndAfter.jsx
@@ -6,11 +6,11 @@ const BeforeAndAfter = () => {
     { url: "/src/assets/BeforeAndAfter/Daniel/1.jpg" },
     { url: "/src/assets/BeforeAndAfter/Daniel/2.jpg" },
     { url: "/src/assets/BeforeAndAfter/Noa/1.jpg" },
-    { url: "/src/assets/BeforeAndAfter/Noa/3.jpg" },
+    { url: "/src/assets/BeforeAndAfter/Noa/2.jpg" },
     { url: "/src/assets/BeforeAndAfter/Limor/1.jpg" },
     { url: "/src/assets/BeforeAndAfter/Limor/2.jpg" },
-    { url: "/src/assets/BeforeAndAfter/Daniel/1.jpg" },
-    { url: "/src/assets/BeforeAndAfter/Daniel/2.jpg" },
+    { url: "/src/assets/BeforeAndAfter/Revital/1.jpg" },
+    { url: "/src/assets/BeforeAndAfter/Revital/2.jpg" },
   ];
 
   return (
