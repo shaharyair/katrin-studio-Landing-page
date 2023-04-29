09dea1ad54fd24867fe70edc90a089cad0910a70
Shahar Yair <shaharyair12@gmail.com>
2023-04-29 22:39:01 +0300

    added vercel url again test

--- src/Components/SignupForm/SignupForm.jsx
+++ src/Components/SignupForm/SignupForm.jsx
@@ -18,7 +18,7 @@ const SignupForm = () => {
   const [submitStatus, setSubmitStatus] = useState(null);
 
   const api = axios.create({
-    baseURL: process.env.API_URL,
+    baseURL: "https://katrin-fitness.vercel.app",
   });
 
   const handleSubmit = async (event) => {
