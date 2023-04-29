6693b4f5bece83f2f5494e40b95edbcd7365c0b5
Shahar Yair <shaharyair12@gmail.com>
2023-04-29 22:04:25 +0300

    change base url to env

--- src/Components/SignupForm/SignupForm.jsx
+++ src/Components/SignupForm/SignupForm.jsx
@@ -18,7 +18,7 @@ const SignupForm = () => {
   const [submitStatus, setSubmitStatus] = useState(null);
 
   const api = axios.create({
-    baseURL: "https://katrinfitness-nodejs-production.up.railway.app/",
+    baseURL: process.env.API_URL,
   });
 
   const handleSubmit = async (event) => {
