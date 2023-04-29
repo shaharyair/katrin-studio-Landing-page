5d342e606b2b32ee41d2e6e6803b661b5148fe8c
Shahar Yair <shaharyair12@gmail.com>
2023-04-29 19:25:50 +0300

    fixed env

--- src/Components/SignupForm/SignupForm.jsx
+++ src/Components/SignupForm/SignupForm.jsx
@@ -18,7 +18,7 @@ const SignupForm = () => {
   const [submitStatus, setSubmitStatus] = useState(null);
 
   const api = axios.create({
-    baseURL: import.meta.REACT_APP_API_URL,
+    baseURL: import.meta.env.VITE_REACT_API_URL,
   });
 
   const handleSubmit = async (event) => {
