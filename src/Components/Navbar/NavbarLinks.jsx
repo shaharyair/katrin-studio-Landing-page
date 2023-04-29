237eb8beb4803a1ef81c40bf4e1e728bc96deb6f
Shahar Yair <shaharyair12@gmail.com>
2023-04-29 16:13:06 +0300

    fixed img and changed scrolllink time to 10000

--- src/Components/Navbar/NavbarLinks.jsx
+++ src/Components/Navbar/NavbarLinks.jsx
@@ -9,7 +9,7 @@ const NavbarLinks = (props) => {
           to='hero'
           spy={true}
           smooth={true}
-          offset={-70}
+          // offset={-70}
           duration={1000}
           className={`${props.navbarLinks.link} ${props.navbarLinks.main}`}
         >
@@ -21,7 +21,7 @@ const NavbarLinks = (props) => {
           to='studio'
           spy={true}
           smooth={true}
-          offset={-70}
+          // offset={-70}
           duration={1000}
           className={props.navbarLinks.link}
         >
