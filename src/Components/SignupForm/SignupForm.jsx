e8d2c6a588ac194bd0a61030bf325fd7fcb15241
Shahar Yair <shaharyair12@gmail.com>
2023-04-30 01:38:38 +0300

    fixed url in signup axios

--- src/Components/SignupForm/SignupForm.jsx
+++ src/Components/SignupForm/SignupForm.jsx
@@ -18,7 +18,7 @@ const SignupForm = () => {
   const [submitStatus, setSubmitStatus] = useState(null);
 
   const api = axios.create({
-    baseURL: "https://katrin-fitness.vercel.app/",
+    baseURL: "https://katrin-fitness-nodejs.vercel.app/",
   });
 
   const handleSubmit = async (event) => {
